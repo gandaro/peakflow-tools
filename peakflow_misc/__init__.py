ebd39ac8fce448d58bd6a7a2c68a3862d59797ce
Jakob Kramer <jakob.kramer@gmx.de>
2015-01-30 13:25:35 +0100

    Add docstrings; add error checks; string formatting

--- peakflow_misc/__init__.py
+++ peakflow_misc/__init__.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 import base64
 import sys
 import urllib2
@@ -11,14 +9,35 @@ from pfpcap import PeakflowBrowser
 from suds.client import Client
 from suds.transport.https import HttpAuthenticated
 
-ARBORWS = 'https://%s/arborws'
+ARBORWS = 'https://{}/arborws'
 
 class PeakflowAPI(object):
+    """Python interface to several Arbor APIs.
+
+    This class can be used to run CLI commands using
+    the SOAP API, to capture packets for a mitigation,
+    and to send requests to the Web Services API.
+    """
+
     def __init__(self, host, username=None, password=None, api_key=None,
                  tms_ip=None):
-        soap_url = 'https://%s/soap/sp' % host
-        wsdl_url = 'file://%s' % pkg_resources.resource_filename(
-            __name__, 'PeakflowSP.wsdl'
+        """Inits PeakflowAPI with API credentials and hostname.
+
+        Args:
+            host: The hostname of the Arbor instance, for example
+                'arbor.example.com'.
+            username: (Optional) Username for SOAP API and accessing
+                the web interface for .pcap download.  This is needed
+                for the download_pcap and cli_run methods.
+            password: (Optional) Password for this username.
+            api_key: (Optional) API key for Web Services API.  This is
+                needed for calls to the post method.
+            tms_ip: (Optional) Hostname of the TMS that shall capture
+                the packets.  Needed for download_pcap.
+        """
+        soap_url = 'https://{}/soap/sp'.format(host)
+        wsdl_url = 'file://{}'.format(
+            pkg_resources.resource_filename(__name__, 'PeakflowSP.wsdl')
         )
 
         if username is None or password is None:
@@ -38,12 +57,39 @@ class PeakflowAPI(object):
         self._timeout = 10
 
     def cli_run(self, command):
+        """Run a command on the Arbor CLI.
+
+        Args:
+            command: The command to be run.
+
+        Returns:
+            The string that is being returned by the SOAP API.
+
+        Example:
+            >>> api.cli_run('foo bar baz')
+            'OK'
+        """
+        if self.client is None:
+            raise RunTimeError('credentials for SOAP API are missing')
         command = base64.b64encode(command)
         result = self.client.service.cliRun(command=command,
                                             timeout=self._timeout)
         return base64.b64decode(result)
 
     def download_pcap(self, mitigation_id, filename):
+        """Capture packets and download them to a .pcap file.
+
+        Args:
+            mitigation_id: The ID of the mitigation that you want to
+                capture packets from.
+            filename: The name of the file you want to download the
+                .pcap file to.
+
+        Example:
+            >>> api.download_pcap(1234, 'foo.pcap')
+        """
+        if self.username is None or self.password is None:
+            raise RunTimeError('credentials for pcap downloading are missing')
         pb = PeakflowBrowser(self.host, self.username, self.password)
         try:
             if not pb.start_flowcapture(mitigation_id, self.tms_ip):
@@ -56,10 +102,25 @@ class PeakflowAPI(object):
             pb.logout()
 
     def post(self, url, **parameters):
+        """Send an HTTP POST request to the Arbor Web Services API.
+
+        Args:
+            url: The part of the API url after the /arborws.
+            **parameters: Any parameters that the API accepts are
+                also accepted as keyword arguments.
+
+        Returns:
+            A requests.Response object for the POST request.
+
+        Example:
+            >>> api.post('/mitigations/status', filter='ongoing', limit=10)
+        """
+        if self.api_key is None:
+            raise RunTimeError('API key is missing')
         data = {
             'api_key': self.api_key
         }
         data.update(parameters)
         # XXX: SSL certificate should be checked
-        return requests.post(ARBORWS % self.host + url, verify=False,
+        return requests.post(ARBORWS.format(self.host + url), verify=False,
                              data=data)
--- rename_mitigations.py
+++ rename_mitigations.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
 import argparse
 import logging
 import re
@@ -8,17 +6,35 @@ import sys
 
 from peakflow_misc import PeakflowAPI
 
-RENAME_COMMAND = 'services sp mitigation tms rename "%s" "%s"'
+RENAME_COMMAND = 'services sp mitigation tms rename "{old}" "{new}"'
 
 logging.basicConfig(filename='/var/log/rename-mitigations.log',
                     format='%(asctime) %(levelname)s: %(message)s')
 
-def rename_mitigation(api, old_name, new_name):
-    sanitize = lambda s: s.replace('"', '').replace('\\', '\\\\')
-    old_name = sanitize(old_name)
-    new_name = sanitize(new_name)
+def sanitize_name(name):
+    """Strip double quotes and escape backslashes in name.
 
-    cmd = RENAME_COMMAND % (old_name, new_name)
+    Args:
+        name: A string that may contain unsafe values for the
+            Peakflow CLI.
+
+    Returns:
+        A string that has double quotes stripped and in which
+        all backslashes have been escaped.
+
+    Example:
+        >>> sanitize_name(r'foo"bar\baz')
+        'foobar\\\\baz'
+    """
+    return name.replace('"', '').replace('\\', '\\\\')
+
+
+def rename_mitigation(api, old_name, new_name):
+    """Rename mitigation."""
+    old_name = sanitize_name(old_name)
+    new_name = sanitize_name(new_name)
+
+    cmd = RENAME_COMMAND.format(old=old_name, new=new_name)
     logging.info('renaming "%s" to "%s"', old_name, new_name)
     return api.cli_run(cmd)
 
@@ -53,7 +69,7 @@ def main():
         managed_object_match = re.match(r'^(.+) \(\d+\)$', managed_object_name)
         managed_object_name = managed_object_match.group(1)
 
-        new_name = '%s (%s)' % (managed_object_name, alert_id)
+        new_name = '{} ({})'.format(managed_object_name, alert_id)
         result = rename_mitigation(api, old_name, new_name)
         if result != 'OK':
             print >>sys.stderr, 'CLI failed:', result
