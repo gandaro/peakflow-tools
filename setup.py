aad0368b4339eb5a8b64ca7161193958428227c7
Jakob Kramer <jakob.kramer@gmx.de>
2015-05-17 02:23:19 +0200

    new pfpcap version

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     include_package_data=True,
     scripts=['pcap_autodl.py', 'rename_mitigations.py'],
     packages=['peakflow_misc'],
-    install_requires=['suds>=0.4', 'requests>=2.5.1', 'pfpcap>=0.1'],
+    install_requires=['suds>=0.4', 'requests>=2.5.1', 'pfpcap>=0.1.1'],
     dependency_links=[
-        'git+https://github.com/gandaro/peakflow-pcap.git@master#egg=pfpcap-0.1'
+        'git+https://github.com/gandaro/peakflow-pcap.git@master#egg=pfpcap-0.1.1'
     ],
     author='Jakob Kramer',
     autor_email='jakob.kramer@gmx.de',
